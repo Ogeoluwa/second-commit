c5b3ccb8c2b152d1fd04f61dd95f828d2238457a
Ogeoluwa <ogeoluwa.otitoloju@gmail.com>
2020-11-02 17:15:41 +0100

    First comit

---
+++ multiple_of_5_and_7.py
@@ -0,0 +1,26 @@
+# my_name = input('Whats your name?')
+# marks = int(input('What did you score?'))
+# if marks >= 90:
+#     print('A*')
+# elif marks >= 80:
+#     print('A')
+# elif marks >=70:
+#     print('B')
+# elif marks  >=60:
+#     print('C')
+# elif marks  >=50:
+#     print('D')
+# else:
+#     print('fail')
+    
+def hello():
+    return "Hello world!"
+
+def func():
+    repeat = input("Type in anything: ")
+    print (repeat)
+
+func()
+def calculator():
+    number = input("Insert a number: ")
+    
